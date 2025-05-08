cd986c4fbc77f1f001807ade7a24a8148d35a86e
Kreditkao <bermutok@gmail.com>
2025-05-09 00:32:19 +0300

    Create Shop.cs

---
+++ Shop.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace Shop
+{
+    public partial class ShopDbContext : DbContext
+    {
+        public ShopDbContext()
+            : base("name=Shop")
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Client> Clients { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Устанавливаем отношение "один ко многим" между заказами и продуктами
+            modelBuilder.Entity<Order>()
+                .HasMany(o => o.Products)
+                .WithMany()
+                .Map(m =>
+                {
+                    m.ToTable("OrderProducts");
+                    m.MapLeftKey("OrderId");
+                    m.MapRightKey("ProductId");
+                });
+
+            // Устанавливаем связь "один к одному" между заказами и клиентами
+            modelBuilder.Entity<Order>()
+                .HasRequired(o => o.Client)
+                .WithMany()
+                .HasForeignKey(o => o.ClientId);
+        }
+    }
+}
